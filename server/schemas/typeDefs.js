04d339a80302ef48c411aa34445171070a3534d5
Zino Ebiala <eebia002@fiu.edu>
2023-08-31 19:11:46 -0400

    Added the ! to make inputs not null

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -14,7 +14,7 @@ const typeDefs = gql`
 
     type User {
         _id: ID!
-        username: String
+        username: String!
         email: String
         bookCount: Int
         savedBooks: [Book]
@@ -24,7 +24,7 @@ const typeDefs = gql`
         bookId: ID!
         authors: [String]
         description: String
-        title: String
+        title: String!
         image: String
         link: String
     }
